77f915c8218a18fa7f058740e9d993fb5531f246
Brian Talma <brian.talma@vtex.com.br>
2020-12-21 13:38:27 -0500

    Skip files that return GatewayTimeout and attempt to process on next loop Log error list by sku

--- dotnet/Models/UpdateResponse.cs
+++ dotnet/Models/UpdateResponse.cs
@@ -9,5 +9,6 @@ namespace DriveImport.Models
         public bool Success { get; set; }
         public string Message { get; set; }
         public string StatusCode { get; set; }
+        public List<string> Results { get; set; }
     }
 }
