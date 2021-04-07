d4f830beaac8998433f9cac0ea4f40134042aa36
Brian Talma <brian.talma@vtex.com.br>
2021-04-07 15:28:08 -0400

    Use the same token for both image and catalog import

--- dotnet/Data/IDriveImportRepository.cs
+++ dotnet/Data/IDriveImportRepository.cs
@@ -7,9 +7,7 @@ namespace DriveImport.Data
 {
     public interface IDriveImportRepository
     {
-        Task<Credentials> GetCredentials();
         Task<Token> LoadToken();
-        Task SaveCredentials(Credentials credentials);
         Task<bool> SaveToken(Token token);
         Task<MerchantSettings> GetMerchantSettings();
         Task SetImportLock(DateTime importStartTime);
